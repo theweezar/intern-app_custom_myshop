14709ef72723b4f3849a323957ca406e08cbed9d
DucHoang <duc.hoang@globee.hk>
2021-08-22 12:57:11 +0700

    Ok

--- cartridges/app_custom_myshop/cartridge/controllers/Account.js
+++ cartridges/app_custom_myshop/cartridge/controllers/Account.js
@@ -3,29 +3,157 @@ var server = require('server');
 var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
 var account = module.superModule;
 server.extend(account);
-// https://dev11-asia-samsonite.demandware.net/on/demandware.store/Sites-RefArchGlobal-Site/en_GB/Login-Show
-server.get('Register', server.middleware.https, csrfProtection.generateToken, function (req, res, next) {
-    var URLUtils = require('dw/web/URLUtils');
-    var Resource = require('dw/web/Resource');
-    var registrationForm = server.forms.getForm('profile');
-    var submitRegistrationUrl = URLUtils.url('Account-SubmitRegistration', 'rurl', '1').relative().toString();
-    var breadcrumbs = [
-        {
-            htmlValue: Resource.msg('global.home', 'common', null),
-            url: URLUtils.home().toString()
-        }
-    ];
-    registrationForm.clear();
-    res.render('account/register', {
-        submitRegistrationUrl: submitRegistrationUrl,
-        registrationForm: registrationForm,
-        breadcrumbs: breadcrumbs
-    });
-    next();
-});
 
-server.append('SubmitRegistration', server.middleware.https, csrfProtection.validateAjaxRequest, function (req, res, next) {
-    next();
-});
+// https://dev11-asia-samsonite.demandware.net/on/demandware.store/Sites-RefArchGlobal-Site/en_GB/Login-Show
+
+server.replace(
+    'SubmitRegistration',
+    server.middleware.https,
+    csrfProtection.validateAjaxRequest,
+    function (req, res, next) {
+        var CustomerMgr = require('dw/customer/CustomerMgr');
+        var Resource = require('dw/web/Resource');
+        var formErrors = require('*/cartridge/scripts/formErrors');
+        var registrationForm = server.forms.getForm('profile');
+
+        // validate unqiue email
+        var existProfile = CustomerMgr.queryProfile('email LIKE {0}', registrationForm.customer.email.value);
+        if (existProfile) {
+            registrationForm.customer.email.valid = false;
+            registrationForm.customer.email.error =
+                Resource.msg('error.message.exist.email', 'forms', null);
+            registrationForm.valid = false;
+        }
+
+        // validate confirm password
+        if (registrationForm.login.password.value
+            !== registrationForm.login.passwordconfirm.value
+        ) {
+            registrationForm.login.password.valid = false;
+            registrationForm.login.passwordconfirm.valid = false;
+            registrationForm.login.passwordconfirm.error =
+                Resource.msg('error.message.mismatch.password', 'forms', null);
+            registrationForm.valid = false;
+        }
+
+        // validate valid password
+        if (!CustomerMgr.isAcceptablePassword(registrationForm.login.password.value)) {
+            registrationForm.login.password.valid = false;
+            registrationForm.login.passwordconfirm.valid = false;
+            registrationForm.login.passwordconfirm.error =
+                Resource.msg('error.message.password.constraints.not.matched', 'forms', null);
+            registrationForm.valid = false;
+        }
+
+        // setting variables for the BeforeComplete function
+        var registrationFormObj = {
+            firstName: registrationForm.customer.firstname.value,
+            lastName: registrationForm.customer.lastname.value,
+            username: registrationForm.customer.username.value,
+            phone: registrationForm.customer.phone.value,
+            email: registrationForm.customer.email.value,
+            gender: registrationForm.customer.gender.value,
+            password: registrationForm.login.password.value,
+            passwordConfirm: registrationForm.login.passwordconfirm.value,
+            validForm: registrationForm.valid,
+            form: registrationForm
+        };
+
+        if (registrationForm.valid) {
+            res.setViewData(registrationFormObj);
+
+            this.on('route:BeforeComplete', function (req, res) { // eslint-disable-line no-shadow
+                var Transaction = require('dw/system/Transaction');
+                var accountHelpers = require('*/cartridge/scripts/helpers/accountHelpers');
+                var authenticatedCustomer;
+                var serverError;
+
+                // getting variables for the BeforeComplete function
+                var registrationForm = res.getViewData(); // eslint-disable-line
+
+                if (registrationForm.validForm) {
+                    var login = registrationForm.username;
+                    var password = registrationForm.password;
+
+                    // attempt to create a new user and log that user in.
+                    try {
+                        Transaction.wrap(function () {
+                            var error = {};
+                            var newCustomer = CustomerMgr.createCustomer(login, password);
+
+                            var authenticateCustomerResult = CustomerMgr.authenticateCustomer(login, password);
+                            if (authenticateCustomerResult.status !== 'AUTH_OK') {
+                                error = { authError: true, status: authenticateCustomerResult.status };
+                                throw error;
+                            }
+
+                            authenticatedCustomer = CustomerMgr.loginCustomer(authenticateCustomerResult, false);
+
+                            if (!authenticatedCustomer) {
+                                error = { authError: true, status: authenticateCustomerResult.status };
+                                throw error;
+                            } else {
+                                // assign values to the profile
+                                var newCustomerProfile = newCustomer.getProfile();
+
+                                newCustomerProfile.firstName = registrationForm.firstName;
+                                newCustomerProfile.lastName = registrationForm.lastName;
+                                newCustomerProfile.phoneMobile = registrationForm.phone;
+                                newCustomerProfile.email = registrationForm.email;
+                                newCustomerProfile.gender = registrationForm.gender;
+                            }
+                        });
+                    } catch (e) {
+                        if (e.authError) {
+                            serverError = true;
+                        } else {
+                            registrationForm.validForm = false;
+                            registrationForm.form.customer.username.valid = false;
+                            registrationForm.form.customer.username.error =
+                                Resource.msg('error.message.username.invalid', 'forms', null);
+                        }
+                    }
+                }
+
+                delete registrationForm.password;
+                delete registrationForm.passwordConfirm;
+                formErrors.removeFormValues(registrationForm.form);
+
+                if (serverError) {
+                    res.setStatusCode(500);
+                    res.json({
+                        success: false,
+                        errorMessage: Resource.msg('error.message.unable.to.create.account', 'login', null)
+                    });
+
+                    return;
+                }
+
+                if (registrationForm.validForm) {
+                    // send a registration email
+                    // accountHelpers.sendCreateAccountEmail(authenticatedCustomer.profile);
+
+                    res.setViewData({ authenticatedCustomer: authenticatedCustomer });
+                    res.json({
+                        success: true,
+                        redirectUrl: accountHelpers.getLoginRedirectURL(req.querystring.rurl, req.session.privacyCache, true)
+                    });
+
+                    req.session.privacyCache.set('args', null);
+                } else {
+                    res.json({
+                        fields: formErrors.getFormErrors(registrationForm)
+                    });
+                }
+            });
+        } else {
+            res.json({
+                fields: formErrors.getFormErrors(registrationForm)
+            });
+        }
+
+        return next();
+    }
+);
 
 module.exports = server.exports();
